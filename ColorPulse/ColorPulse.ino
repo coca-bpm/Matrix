a3f7467ba498f93b0977cbfc24439f96756633ca
Sichen Liu <sichenn@outlook.com>
2019-04-18 22:56:49 -0700

    added ColorPuls

---
+++ ColorPulse/ColorPulse.ino
@@ -0,0 +1,133 @@
+/* This example shows how to display a color whose brightness depends on the 
+ * heart beat
+
+/* By default, the APA102 library uses pinMode and digitalWrite
+ * to write to the LEDs, which works on all Arduino-compatible
+ * boards but might be slow.  If you have a board supported by
+ * the FastGPIO library and want faster LED updates, then install
+ * the FastGPIO library and uncomment the next two lines: */
+// #include <FastGPIO.h>
+// #define APA102_USE_FAST_GPIO
+
+#include <APA102.h>
+
+// Define which pins to use.
+const uint8_t dataPin = 11;
+const uint8_t clockPin = 12;
+
+// Create an object for writing to the LED strip.
+APA102<dataPin, clockPin> ledStrip;
+
+// Set the number of LEDs to control.
+const uint16_t ledCount = 64;
+
+// Create a buffer for holding the colors (3 bytes per color).
+rgb_color colors[ledCount];
+
+// Set the brightness to use (the maximum is 31).
+uint8_t brightness = 1;
+
+//  Variables
+
+int PulseSensorPurplePin = 0; // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+int LED13 = 13;               //  The on-board Arduion LED
+
+int maxBrightness = 15;
+int Signal;          // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 550; // Determine which Signal to "count as a beat", and which to ingore.
+const int Bound = 1024;
+
+void setup()
+{
+  pinMode(LED13, OUTPUT); // pin that will blink to your heartbeat!
+  Serial.begin(9600);     // Set's up Serial Communication at certain speed.
+}
+
+/* Converts a color from HSV to RGB.
+ * h is hue, as a number between 0 and 360.
+ * s is the saturation, as a number between 0 and 255.
+ * v is the value, as a number between 0 and 255. */
+rgb_color hsvToRgb(uint16_t h, uint8_t s, uint8_t v)
+{
+  uint8_t f = (h % 60) * 255 / 60;
+  uint8_t p = (255 - s) * (uint16_t)v / 255;
+  uint8_t q = (255 - f * (uint16_t)s / 255) * (uint16_t)v / 255;
+  uint8_t t = (255 - (255 - f) * (uint16_t)s / 255) * (uint16_t)v / 255;
+  uint8_t r = 0, g = 0, b = 0;
+  switch ((h / 60) % 6)
+  {
+  case 0:
+    r = v;
+    g = t;
+    b = p;
+    break;
+  case 1:
+    r = q;
+    g = v;
+    b = p;
+    break;
+  case 2:
+    r = p;
+    g = v;
+    b = t;
+    break;
+  case 3:
+    r = p;
+    g = q;
+    b = v;
+    break;
+  case 4:
+    r = t;
+    g = p;
+    b = v;
+    break;
+  case 5:
+    r = v;
+    g = p;
+    b = q;
+    break;
+  }
+  return rgb_color(r, g, b);
+}
+
+void loop()
+{
+  readPulseData();
+  displaySignalOnLed();
+  displayMatrix();
+  delay(10);
+}
+
+void readPulseData()
+{
+  Signal = analogRead(PulseSensorPurplePin); // Read the PulseSensor's value.
+                                             // Assign this value to the "Signal" variable.
+}
+
+void displaySignalOnLed()
+{
+  Serial.println(Signal); // Send the Signal value to Serial Plotter.
+  if (Signal > Threshold)
+  { // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+    digitalWrite(LED13, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED13, LOW); //  Else, the sigal must be below "550", so "turn-off" this LED.
+  }
+}
+
+void displayMatrix()
+{
+  uint8_t time = millis() >> 4;
+
+  for (uint16_t i = 0; i < ledCount; i++)
+  {
+    uint8_t p = time - i * 8;
+    
+    colors[i] = hsvToRgb(0, 100, 100);
+  }
+
+  brightness = constrain(Signal / Threshold, 0, 1) * maxBrightness;
+  ledStrip.write(colors, ledCount, brightness);
+}
